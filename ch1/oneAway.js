28c7184629060d172b38bf9740f9c55677a441b6
Tokki0401 <gyuhun95@gmail.com>
2020-08-03 14:33:09 -0700

    oneAway problem

---
+++ ch1/oneAway.js
@@ -0,0 +1,9 @@
+// There are three types of edits that can be performed on strings: insert, remove, replace a character. Given two strings, write a function to check if they are one eidt (or zero edits) away
+
+// Ex.
+// pale, ple -> true
+// pale, bake -> false
+
+function oneAway() {
+
+}
