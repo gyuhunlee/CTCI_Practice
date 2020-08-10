7b710ac4890cc57dc603f723c4154eb0cf0ec926
Tokki0401 <gyuhun95@gmail.com>
2020-08-10 09:46:23 -0700

    stringRotation

---
+++ ch1/stringRotation.js
@@ -0,0 +1,36 @@
+// Assume you have a method isSubstring which checks if one word is a substring of another.
+// Given s1, s2, write code to check if s2 is a rotation of s1
+
+// Ex. "waterbottle" is a rotation of "erbottlewat"
+
+function isSubstring(str1, str2) {
+  // return false if str1 length is not equal to str2
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  var arrayStr2 = str2.split('');
+
+  // loop through str2 for the length of str2
+  for (let i = 0; i < str2.length; i++) {
+    // if str1 is equal to str2
+    if (str1 === str2) {
+      // return true
+      return true;
+    } else {
+      // otherwise get the first letter from str2 and push it to the end
+      var rotate = arrayStr2.shift();
+      arrayStr2.push(rotate);
+      // reassign str2
+      str2 = arrayStr2.join('');
+    }
+  }
+
+  // return false
+  return false;
+}
+
+console.log(isSubstring("waterbottle", "erbottlewat"));
+console.log(isSubstring("ete", "tee"));
+console.log(isSubstring("acac", "caca"));
+console.log(isSubstring("cactus", "totoros"));
